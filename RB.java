c225a4f2db48f1bff610a9a1f62c680e6fb028b9
Navya-Hebbar <navyahebbar032005@gmail.com>
2023-08-30 13:01:32 +0530

    updated

--- RB.java
+++ RB.java
@@ -5,7 +5,8 @@ class RB
 			System.out.println("Happy RakshaBhandana");
 			for(int i=1;i<=10;i++)
 			{
-				System.out.println("Keep Smiling Brigther")
+				System.out.println("Keep Smiling Brigther");
 			}
+			System.out.println("Thank you");
 		}
 }
