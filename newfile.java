d3a6dc17ed85d3286c5ad67284a75ced7e488ed5
Navya-Hebbar <navyahebbar032005@gmail.com>
2023-08-31 10:25:20 +0530

    Adding a new java file

---
+++ newfile.java
@@ -0,0 +1,13 @@
+class newfile
+{
+	public static void main(String [] args)
+		{
+			System.out.println("Good Morning");
+			for(int i=1;i<=10;i++)
+			{
+				System.out.println("Have a great day");
+			}
+			System.out.println("Today is Thursday");
+			System.out.println("End of the program");
+		}
+}
